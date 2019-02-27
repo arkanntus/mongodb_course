a14c4d2d2a7b9689f26b2ff1d97666e524593d7d
Anderson M. de Almeida <arkanntus@gmail.com>
2019-02-26 21:01:42 -0300

    The Increment Update Operator

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -4,7 +4,7 @@ const User = require('../src/user');
 describe('Updating records', () => {
   let joe;
   beforeEach((done) => {
-    joe = new User({ name: 'Joe'});
+    joe = new User({ name: 'Joe', postCount: 0});
     joe.save()
       .then(() => done());    
   });
@@ -47,5 +47,14 @@ describe('Updating records', () => {
       User.findByIdAndUpdate(joe._id, { name: 'Alex' }),
       done
     );
+  });
+
+  it ('A user can have thier poscount incremented by 1', (done) => {
+    User.update({ name: 'Joe'}, { $inc: { postCount: 1 } })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.postCount === 1);
+        done();
+      });
   });
 });
