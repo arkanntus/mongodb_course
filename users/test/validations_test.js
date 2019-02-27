2826d1ed0f4b750a86a79cb0ec970f777504ebc8
Anderson M. de Almeida <arkanntus@gmail.com>
2019-02-26 21:26:15 -0300

    Validation With a Validator Function

--- users/test/validations_test.js
+++ users/test/validations_test.js
@@ -9,4 +9,12 @@ describe('Validating records', () => {
     const { message } = validationResult.errors.name; //ES6
     assert( message === 'Name is required.');
   });
+
+  it ('requires a user name longer then 2 characters', () => {
+    const user = new User({ name: 'Al' });
+    const validationResult = user.validateSync();
+    const { message } = validationResult.errors.name;
+
+    assert( message === 'Name must be longer than 2 characters.' );
+  });
 });
