30a05b8460eb744a5d0eff08dc317888892ce73f
Anderson M. de Almeida <arkanntus@gmail.com>
2019-02-28 21:43:05 -0300

    Fixing Update Tests

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -4,7 +4,7 @@ const User = require('../src/user');
 describe('Updating records', () => {
   let joe;
   beforeEach((done) => {
-    joe = new User({ name: 'Joe', postCount: 0});
+    joe = new User({ name: 'Joe', likes: 0});
     joe.save()
       .then(() => done());    
   });
@@ -49,11 +49,11 @@ describe('Updating records', () => {
     );
   });
 
-  xit ('A user can have thier poscount incremented by 1', (done) => {
-    User.update({ name: 'Joe'}, { $inc: { postCount: 1 } })
+  it ('A user can have their likes incremented by 1', (done) => {
+    User.update({ name: 'Joe'}, { $inc: { likes: 10 } })
       .then(() => User.findOne({ name: 'Joe' }))
       .then((user) => {
-        assert(user.postCount === 1);
+        assert(user.likes === 10);
         done();
       });
   });
