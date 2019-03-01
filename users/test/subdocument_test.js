0aa042e6ca1351583f263340836fbeb5873662d0
Anderson M. de Almeida <arkanntus@gmail.com>
2019-02-28 21:09:00 -0300

    Adding Subdocuments to Existing Records

--- users/test/subdocument_test.js
+++ users/test/subdocument_test.js
@@ -9,10 +9,29 @@ describe('Subdocuments', () => {
     });
 
     joe.save()
-    .then(() => User.findOne({ name: 'Joe'}))
-    .then((user) => {
-      assert(user.posts[0].title === 'PostTitle');
-      done();
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.posts[0].title === 'PostTitle');
+        done();
+      });
+  });
+
+  it ('Can add subdocuments to an existing record', (done) => {
+    const joe = new User({ 
+      name: 'Joe', 
+      posts: []
     });
+
+    joe.save()
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        user.posts.push({ title: 'New Post' });
+        return user.save();
+      })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.posts[0].title === 'New Post');
+        done();
+      });
   });
 });
