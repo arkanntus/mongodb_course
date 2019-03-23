38f01c25cfeada27bf6f996a4b03cacde1d6f9f7
Anderson M. de Almeida <arkanntus@gmail.com>
2019-03-23 11:00:21 -0300

    Filtering with Multiple Props

--- UpStarMusic/database/queries/SearchArtists.js
+++ UpStarMusic/database/queries/SearchArtists.js
@@ -9,7 +9,8 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
-  const query = Artist.find({})
+  const query = Artist
+    .find(buildQuery(criteria))
     .sort({ [sortProperty]: 1 })
     .skip(offset)
     .limit(limit);
@@ -24,3 +25,23 @@ module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
       };
     });
 };
+
+const buildQuery = (criteria) => {
+  const query = {};
+
+  if (criteria.age) {
+    query.age = {
+      $gte: criteria.age.min,
+      $lte: criteria.age.max
+    };
+  }
+
+  if (criteria.yearsActive) {
+    query.yearsActive = {
+      $gte: criteria.yearsActive.min,
+      $lte: criteria.yearsActive.max
+    };
+  }
+
+  return query;
+}
