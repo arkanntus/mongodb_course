0c9539e0780b500c3d53b42d6d1c19f81e64fae6
Anderson M. de Almeida <arkanntus@gmail.com>
2019-04-07 10:38:43 -0300

    Testing GeoQueries

--- muber/controllers/drivers_controller.js
+++ muber/controllers/drivers_controller.js
@@ -5,6 +5,50 @@ module.exports = {
     res.send({ hi: 'there' });
   },
 
+  index(req, res, next) {
+    const { lng, lat } = req.query;
+    const point = {
+        type: 'Point',
+        coordinates: [parseFloat(lng), parseFloat(lat)],
+    };
+    Driver.aggregate([
+        {
+            $geoNear: { 
+                near: point,
+                spherical: true,
+                maxDistance: 200000,
+                distanceField: 'dist.calculated'
+            }
+        }])
+        .then((drivers) => {
+            res.send(drivers);
+        })
+        .catch(next);
+
+
+    // const { lng, lat } = req.query;
+    // Driver.find({
+    //   'geometry.coordinates': {
+    //       $nearSphere: {
+    //           $geometry: {
+    //               type: "Point",
+    //               coordinates: [lng, lat]
+    //           },
+    //           $maxDistance: 200000 //200km
+    //       }
+    //   }
+    // })
+    //   .then(drivers => res.send(drivers))
+    //   .catch(next);
+
+    // Driver.geoNear(
+    //   { type: 'Point', coordinates: [parseFloat(lng), parseFloat(lat)] },
+    //   { spherical: true, maxDistance: 200000 } //200km
+    // )
+    //   .then(drivers => res.send(drivers))
+    //   .catch(next);
+  },
+
   create(req, res, next) {
     const driverProps = req.body;
     
